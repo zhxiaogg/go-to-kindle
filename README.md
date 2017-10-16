5a7d0c21a42d29e4dcefa8d2a56559ec02560338
zhxiaog <zhxiaog@outlook.com>
2017-10-16 23:31:30 +0800

    can send pdf now

---
+++ README.md
@@ -0,0 +1,3 @@
+# send to kindle
+
+convert a url to pdf and send it to your mailbox(eg. kindle).
--- functions.js
+++ functions.js
@@ -1,28 +1,92 @@
 const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
 
-process.env.LD_LIBRARY_PATH = process.env['FC_FUNC_CODE_PATH'] + 'node_modules/nss/lib/' +  ':' + process.env.LD_LIBRARY_PATH;
-process.env.PATH = process.env['FC_FUNC_CODE_PATH'] + 'node_modules/nss/bin/' + ':' + process.env.PATH;
+let mailer = nodemailer.createTransport({
+  host: 'smtpdm.aliyun.com',
+  port: 25,
+  secureConnection: true,
+  auth: {
+     user: 'noreply@send.gotokindle.com',
+     pass: 'Send2017Kindle'
+  }
+});
 
-module.exports.print_image = function(event, context, callback) {
+const CODE_PATH = process.env['FC_FUNC_CODE_PATH'];
+
+fs.mkdir(`/tmp/pdfs`, (err) => {
+  if(err) {
+    console.log(err);
+  }
+});
+
+// set library load path to resolve libnss3.so
+process.env.LD_LIBRARY_PATH =  `${CODE_PATH}node_modules/nss/lib/`;
+
+module.exports.send = function(event, context, callback) {
   const {url, mail} = JSON.parse(event.toString());
-  console.log("code path: ", process.env['FC_FUNC_CODE_PATH']);
-  console.log("ld path:", process.env['LD_LIBRARY_PATH']);
+  const {requestId} = context;
+
+  const pdf_path = `/tmp/pdfs/${requestId}.pdf`;
+
+  const options = {
+    path: pdf_path,
+    mail: mail,
+    url: url,
+    requestId: requestId
+  }
+
   puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: process.env['FC_FUNC_CODE_PATH'] + 'node_modules/headless-chrome/headless_shell'
+    // executablePath: `${CODE_PATH}node_modules/headless-chrome/headless_shell`
+    executablePath: '/Users/zhxiaog/Downloads/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
   }).then(browser => {
-    return browser.newPage().then(page => {
-      return page.goto(url).then(() => {
-        return page.screenshot({path: '/tmp/asdf.png'});
-      });
-    }).then(() => {
-      return browser.close();
-    }).then(() => {
-      callback(null, "ok");
-    }).catch(e => {
+    return print(browser, options).then(() => {
+      browser.close();
+    });
+  }).then(() => {
+    callback(null, "ok");
+  }).catch(e => {
       console.log(e);
       callback(new Error(e), "failed");
-    });
   });
 };
+
+function print(browser, {url, mail, path, requestId}) {
+  const promise = browser.newPage().then(page => {
+    return page.goto(url).then(() => {
+      page.pdf({
+        path: path,
+        printBackground: false,
+      })
+    }).then(() => {
+      // send pdf to mail
+      const mailOptions = {
+          from: 'gotokindle<noreply@send.gotokindle.com>',
+          to: mail, // list of receivers
+          subject: 'your sendtokindle service',
+          text: 'Best Regards!',
+          // html: '<b>Hello world</b><img src="cid:01" style="width:200px;height:auto">', // html body
+          attachments: [
+              {
+                  filename: `${requestId}.pdf`,
+                  path: path
+              }
+          ],
+      };
+
+      const result = new Promise((resolve, reject) => {
+        mailer.sendMail(mailOptions, (error, info) => {
+          if(error) {
+            reject(error);
+          } else {
+            resolve(info);
+          }
+        });
+      });
+      return result;
+    })
+  });
+  return promise;
+}
